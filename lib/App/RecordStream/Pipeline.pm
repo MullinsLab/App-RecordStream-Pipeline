6cea74383e0c410ddcbb24755bc8e6cde9c8fcd6
Evan Silberman <silby@uw.edu>
2017-01-26 16:43:19 -0800

    Rework Pipeline for method-chaining interface
    Implements a pleasing method-chaining interface for building up recs
    pipelines in Perl code.

--- lib/App/RecordStream/Pipeline.pm
+++ lib/App/RecordStream/Pipeline.pm
@@ -3,43 +3,100 @@ use warnings;
 use utf8;
 
 package App::RecordStream::Pipeline;
-use parent 'Exporter::Tiny';
 
+use Moo;
+extends 'Exporter::Tiny';
+use App::RecordStream;
 use App::RecordStream::Pipeline::Operation;
 use App::RecordStream::Pipeline::Sink::ArrayRef;
 use App::RecordStream::Pipeline::Sink::FileHandle;
-use IO::String;
+use List::Util qw< reduce >;
 use Types::Standard qw< :types >;
 use namespace::clean;
 
-our @EXPORT = qw[ recs sink ];
+our @EXPORT = qw< recs >;
+
+my @operations = map { s/^App::RecordStream::Operation:://; $_ }
+                 App::RecordStream->operation_packages;
+for my $op (@operations) {
+    no strict "refs";
+    *{"$op"} = sub { my $self = shift; $self->_chain_operation($op, @_); };
+}
 
 sub recs {
-    my ($name, $args, $next) = @_;
-    App::RecordStream::Pipeline::Operation->new(
-        name => $name,
-        args => $args,
-        ($next
-            ? (next => $next)
-            : ()),
+    App::RecordStream::Pipeline->new
+};
+
+has pipeline => (
+    is      => 'ro',
+    isa     => ArrayRef,
+    default => sub { [] },
+);
+
+sub then {
+    my $self = shift;
+    my $next = shift;
+    return App::RecordStream::Pipeline->new(
+        pipeline => [ @{$next->pipeline}, @{$self->pipeline} ]
     );
 }
 
-sub sink {
-    my $output = shift;
+sub _chain_operation {
+    my $self = shift;
+    my $name = shift;
+    my $op = { name => $name, args => \@_ };
+    App::RecordStream::Pipeline->new(
+        pipeline => [ $op, @{$self->pipeline} ]
+    );
+}
 
-    $output = IO::String->new($output)
-        if ScalarRef->check($output);
+sub run {
+    my $self = shift;
+    my %params = @_;
 
-    return App::RecordStream::Pipeline::Sink::FileHandle->new( handle => $output )
-        if FileHandle->check($output);
+    # Set up a sink and a callback to return the desired value based on
+    # the end of the pipeline.
+    my $sink;
+    my $return;
+    my $final = $self->pipeline->[0];
 
-    return App::RecordStream::Pipeline::Sink::ArrayRef->new( records => $output )
-        if ArrayRef->check($output);
+    if ($params{output} && FileHandle->check($params{output})) {
+        # If given an output filehandle, write to it and return the filehandle,
+        # no matter what's in the pipeline
+        $sink = App::RecordStream::Pipeline::Sink::FileHandle->new( handle => $params{output} );
+        $return = sub { $params{output} };
+    } elsif ($final->{name} =~ /^to(?!pn$)/) {
+        # If the pipeline ends with a "to$format" operation, we're probably going
+        # to get a string, so accumulate the results as a string
+        my $result;
+        open my $io, ">", \$result or die "Unable to open handle: $!";
+        $sink = App::RecordStream::Pipeline::Sink::FileHandle->new( handle => $io );
+        $return = sub {
+            close $io or die "Unable to close handle: $!";
+            $result
+        };
+    } else {
+        # Otherwise, there's some intermediate pipeline step at the end, and we'll
+        # just return a list of hashrefs (i.e. records).
+        my @result;
+        $sink = App::RecordStream::Pipeline::Sink::ArrayRef->new( records => \@result );
+        $return = sub { @result };
+    }
 
-    # Assume they know what they're doing... maybe a custom object.  The type
-    # checking in App::RecordStream::Pipeline::Operation will catch problems.
-    return $output;
+    # Build up the pipeline operations, ending with the sink we chose.
+    my $compiled = reduce {
+        App::RecordStream::Pipeline::Operation->new(
+            name => $b->{name},
+            args => $b->{args},
+            ($a
+                ? (next => $a)
+                : ()),
+        );
+    } $sink, @{$self->pipeline};
+
+    # Pass along any input parameter to the initial operation.
+    $compiled->run($params{input} ? $params{input} : ());
+    $return->();
 }
 
 1;
